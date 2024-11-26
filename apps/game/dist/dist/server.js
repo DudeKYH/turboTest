3c1dcb1d23d394a1ce03438abd10ede4f269cd9c
Young-Hyun Kwon <ehe151@naver.com>
2024-11-26 12:53:00 +0900

    add : commit reset turbo & babel
    add : commit reset turbo & babel

---
+++ apps/game/dist/dist/server.js
@@ -0,0 +1,11 @@
+import net from "net";
+import { onConnection } from "./events/onConnection.js";
+import { Person } from "@turbo-test/classes";
+const gameServer = net.createServer(onConnection);
+const PORT = 5555;
+const HOST = "127.0.0.1";
+gameServer.listen(5555, "127.0.0.1", () => {
+  console.log(`[GameServer] ${HOST}:${PORT} listening`);
+  const person = new Person(25, 180);
+  person.grow();
+});
